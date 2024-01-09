65d97691ca441ac0b4eb4f50350f78f31f692afe
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-01-08 17:06:23 -0800

    :sparkles: test render

--- plugin.py
+++ plugin.py
@@ -13,9 +13,35 @@ bl_info = {
 
 import bpy
 
+from bpy.props import (StringProperty,
+                       BoolProperty,
+                       IntProperty,
+                       FloatProperty,
+                       FloatVectorProperty,
+                       EnumProperty,
+                       PointerProperty,
+                       )
+from bpy.types import (Panel,
+                       Menu,
+                       Operator,
+                       PropertyGroup,
+                       )
+
+# Global plugin properties
+class PluginProperties(PropertyGroup):
+
+    test_resolution: IntProperty(
+        name = "Test Resolution",
+        description="Temporary resolution for test renders",
+        default = 50,
+        min = 10,
+        max = 100
+        )
+
+# The UI Panel in Render View
 class ExportPresetsPanel(bpy.types.Panel):
     """Creates a Panel in the scene context of the properties editor"""
-    bl_label = "Export Presets"
+    bl_label = "Render Buddy"
     bl_idname = "SCENE_PT_layout"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
@@ -25,13 +51,17 @@ class ExportPresetsPanel(bpy.types.Panel):
         layout = self.layout
 
         scene = context.scene
+        state = scene.render_buddy
 
         
         # Big render button
         layout.label(text="Test Render:")
         row = layout.row()
-        row.operator("render.render")
+        row.operator("export_options.test_render")
+        row = layout.row()
+        row.prop(state, "test_resolution")
 
+        layout.label(text="Export Presets")
         # Create two columns, by using a split layout.
         split = layout.split()
 
@@ -58,8 +88,8 @@ class EXPORT_OPTIONS_set_1080p_square(bpy.types.Operator):
     bl_options = {"UNDO"}
 
     def execute(self, context: bpy.types.Context) -> set[str]:
-        bpy.data.scenes["Scene"].render.resolution_x = 1080;
-        bpy.data.scenes["Scene"].render.resolution_y = 1080;
+        bpy.context.scene.render.resolution_x = 1080;
+        bpy.context.scene.render.resolution_y = 1080;
         return {"FINISHED"}
 
 class EXPORT_OPTIONS_set_1080p_vertical(bpy.types.Operator):
@@ -69,8 +99,8 @@ class EXPORT_OPTIONS_set_1080p_vertical(bpy.types.Operator):
     bl_options = {"UNDO"}
 
     def execute(self, context: bpy.types.Context) -> set[str]:
-        bpy.data.scenes["Scene"].render.resolution_x = 1080;
-        bpy.data.scenes["Scene"].render.resolution_y = 1920;
+        bpy.context.scene.render.resolution_x = 1080;
+        bpy.context.scene.render.resolution_y = 1920;
         return {"FINISHED"}
 
 class EXPORT_OPTIONS_set_1080p_widescreen(bpy.types.Operator):
@@ -80,8 +110,8 @@ class EXPORT_OPTIONS_set_1080p_widescreen(bpy.types.Operator):
     bl_options = {"UNDO"}
 
     def execute(self, context: bpy.types.Context) -> set[str]:
-        bpy.data.scenes["Scene"].render.resolution_x = 1920;
-        bpy.data.scenes["Scene"].render.resolution_y = 1080;
+        bpy.context.scene.render.resolution_x = 1920;
+        bpy.context.scene.render.resolution_y = 1080;
         return {"FINISHED"}
 
 class EXPORT_OPTIONS_set_4k_square(bpy.types.Operator):
@@ -91,8 +121,8 @@ class EXPORT_OPTIONS_set_4k_square(bpy.types.Operator):
     bl_options = {"UNDO"}
 
     def execute(self, context: bpy.types.Context) -> set[str]:
-        bpy.data.scenes["Scene"].render.resolution_x = 3840;
-        bpy.data.scenes["Scene"].render.resolution_y = 3840;
+        bpy.context.scene.render.resolution_x = 3840;
+        bpy.context.scene.render.resolution_y = 3840;
         return {"FINISHED"}
 
 class EXPORT_OPTIONS_set_4k_widescreen(bpy.types.Operator):
@@ -102,8 +132,8 @@ class EXPORT_OPTIONS_set_4k_widescreen(bpy.types.Operator):
     bl_options = {"UNDO"}
 
     def execute(self, context: bpy.types.Context) -> set[str]:
-        bpy.data.scenes["Scene"].render.resolution_x = 3840;
-        bpy.data.scenes["Scene"].render.resolution_y = 2160;
+        bpy.context.scene.render.resolution_x = 3840;
+        bpy.context.scene.render.resolution_y = 2160;
         return {"FINISHED"}
 
 class EXPORT_OPTIONS_set_4k_vertical(bpy.types.Operator):
@@ -113,12 +143,52 @@ class EXPORT_OPTIONS_set_4k_vertical(bpy.types.Operator):
     bl_options = {"UNDO"}
 
     def execute(self, context: bpy.types.Context) -> set[str]:
-        bpy.data.scenes["Scene"].render.resolution_x = 2160;
-        bpy.data.scenes["Scene"].render.resolution_y = 3840;
+        bpy.context.scene.render.resolution_x = 2160;
+        bpy.context.scene.render.resolution_y = 3840;
+        return {"FINISHED"}
+
+class EXPORT_OPTIONS_test_render(bpy.types.Operator):
+    bl_idname = "export_options.test_render"
+    bl_label = "Test Render"
+    bl_description = "Renders at 50% then returns prev. settings"
+
+    def execute(self, context: bpy.types.Context) -> set[str]:
+        # Preserve prev settings
+        # Make sure to copy any properties of classes
+        import copy
+        prev_reso_percent = copy.copy(bpy.context.scene.render.resolution_percentage)
+        prev_file_path = copy.copy(bpy.context.scene.render.filepath)
+        
+        # Set to 50%
+        bpy.context.scene.render.resolution_percentage = 50
+
+        # Make a filename based on current `.blend` file
+        import os
+        output_dir = os.path.dirname(bpy.data.filepath)
+        filename_dirty = os.path.basename(bpy.data.filepath)
+        filename = filename_dirty.replace(".blend", "")
+        file_type = bpy.context.scene.render.image_settings.file_format
+        output_file_pattern_string = filename + "-%s." + file_type
+
+        # Get the current time to append to filename
+        from datetime import datetime
+        now = datetime.now() # current date and time
+        time = now.strftime("%H-%M-%S")
+
+        # Render and save to file
+        bpy.context.scene.render.filepath = os.path.join(output_dir, (output_file_pattern_string % time))
+        bpy.ops.render.render(write_still = bpy.data.is_saved)
+
+        # Return settings
+        bpy.context.scene.render.resolution_percentage = prev_reso_percent
+        bpy.context.scene.render.filepath = prev_file_path
+        
         return {"FINISHED"}
 
 classes = (
+    PluginProperties,
     ExportPresetsPanel,
+    EXPORT_OPTIONS_test_render,
     EXPORT_OPTIONS_set_1080p_square,
     EXPORT_OPTIONS_set_1080p_vertical,
     EXPORT_OPTIONS_set_1080p_widescreen,
@@ -131,12 +201,16 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
     # bpy.utils.register_classes_factory(classes)
+        
+    bpy.types.Scene.render_buddy = PointerProperty(type=PluginProperties)
 
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
     # bpy.utils.register_classes_factory(classes)
+        
+    del bpy.types.Scene.render_buddy
 
 
 if __name__ == "__main__":
